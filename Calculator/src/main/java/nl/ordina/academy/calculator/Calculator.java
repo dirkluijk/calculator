959b27bfb4c25c16020e1ce50dbfd92a9265383b
Dirk Luijk <dirk@luijkwebcreations.nl>
2015-05-15 16:29:52 +0200

    Fixed broken code

--- Calculator/src/main/java/nl/ordina/academy/calculator/Calculator.java
+++ Calculator/src/main/java/nl/ordina/academy/calculator/Calculator.java
@@ -8,27 +8,27 @@ import nl.ordina.academy.calculator.exception.EmptyInputException;
  * Calculator used to do mathematical operations.
  */
 public class Calculator {
-    private Tokenizer tokenizer;
+    private MathTokenizer tokenizer;
     private RPNConverter converter;
     private RPNInterpreter interpreter;
 
     /**
      * Constructor creating an instance with an specific tokenizer, RPN converter and interpreter.
-     * @param tokenizer {@link Tokenizer} specific tokeneizer to format the input
+     * @param tokenizer {@link MathTokenizer} specific tokeneizer to format the input
      * @param converter {@link RPNConverter} used to convert input to RPN notation for enhanced calculations
      * @param interpreter {@link RPNInterpreter} used to do the RPN calculations
      */
-    public Calculator(Tokenizer tokenizer, RPNConverter converter, RPNInterpreter interpreter) {
+    public Calculator(MathTokenizer tokenizer, RPNConverter converter, RPNInterpreter interpreter) {
         this.tokenizer = tokenizer;
         this.converter = converter;
         this.interpreter = interpreter;
     }
 
     /**
-     * Default constructor which init and use default {@link Tokenizer}, {@link RPNConverter} and {@link RPNInterpreter} classes.
+     * Default constructor which init and use default {@link MathTokenizer}, {@link RPNConverter} and {@link RPNInterpreter} classes.
      */
     public Calculator() {
-        this(new Tokenizer(), new RPNConverter(), new RPNInterpreter());
+        this(new MathTokenizer(), new RPNConverter(), new RPNInterpreter());
     }
 
     /**
@@ -40,7 +40,7 @@ public class Calculator {
      */
     public BigDecimal calculate(String input) throws CalculatorException {
 
-        if (input.isEmpty()) {
+        if (input.trim().isEmpty()) {
             throw new EmptyInputException("Input may not be empty.");
         }
 
